ccfe4f09b14181cc7aae8eddd33e47c650251e61
Lisa Lee <lisa_lee@brown.edu>
2017-09-23 00:25:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+require 'pry'
+def countdown(num)
+  counter = num
+  while counter > 0
+    counter -= 1
+    puts "#{counter} SECOND(S)!"
 
-def countdown
+  end
+  puts "HAPPY NEW YEAR!"
+  binding.pry
 end
