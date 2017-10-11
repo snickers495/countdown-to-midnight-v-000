6c15acc9a5b7ca32b70e261b1cdace3fc4f8ee4f
Lisa Lee <snickers495@gmail.com>
2017-10-11 15:47:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,19 @@ def countdown(num)
   counter = num - 1
   while counter > 0
     counter -= 1
-    sleep 1
     puts "#{counter} SECOND(S)!\n"
+
+  end
+  return "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(num)
+  counter = num - 1
+  while counter > 0
+    counter -= 1
+    puts "#{counter} SECOND(S)!\n"
+    sleep 1
 
   end
   return "HAPPY NEW YEAR!"
